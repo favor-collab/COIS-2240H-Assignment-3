a4e5cf4b1e81444e54864dcc45c5efba51f28571
Anna Ogunkolade <giftedfavor12@gmail.com>
2025-11-26 22:36:50 -0500

    Refactor Vehicle constructor to use private capitalize() helper

--- RentalSystem.java
+++ RentalSystem.java
@@ -6,7 +6,7 @@ import java.io.BufferedWriter;
 import java.io.PrintWriter;
 import java.io.FileReader;
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.IOException; xx
 
 public class RentalSystem {
 
