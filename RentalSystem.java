ab783bffe72c73522e4a0f04bfda3d1383ea810c
Anna Ogunkolade <giftedfavor12@gmail.com>
2025-11-27 14:47:05 -0500

    Fixed VehicleRentalTest and updated RentalSystem to support tests

--- RentalSystem.java
+++ RentalSystem.java
@@ -117,30 +117,35 @@ public class RentalSystem {
         }
     }
 
-    // ------------------ RENTAL ------------------
-    public void rentVehicle(Vehicle vehicle, Customer customer, LocalDate date, double amount) {
+ // ------------------ RENTAL ------------------
+    public boolean rentVehicle(Vehicle vehicle, Customer customer, LocalDate date, double amount) {
         if (vehicle.getStatus() == Vehicle.VehicleStatus.Available) {
             vehicle.setStatus(Vehicle.VehicleStatus.Rented);
             RentalRecord record = new RentalRecord(vehicle, customer, date, amount, "RENT");
             rentalHistory.add(record);
             saveRecord(record);
             System.out.println("Vehicle rented to " + customer.getCustomerName());
+            return true;
         } else {
             System.out.println("Vehicle is not available.");
+            return false;
         }
     }
 
-    public void returnVehicle(Vehicle vehicle, Customer customer, LocalDate date, double extraFees) {
+    public boolean returnVehicle(Vehicle vehicle, Customer customer, LocalDate date, double extraFees) {
         if (vehicle.getStatus() == Vehicle.VehicleStatus.Rented) {
             vehicle.setStatus(Vehicle.VehicleStatus.Available);
             RentalRecord record = new RentalRecord(vehicle, customer, date, extraFees, "RETURN");
             rentalHistory.add(record);
             saveRecord(record);
             System.out.println("Vehicle returned by " + customer.getCustomerName());
+            return true;
         } else {
             System.out.println("Vehicle is not rented.");
+            return false;
         }
     }
+
 
     public void displayRentalHistory() {
         if (rentalHistory.isEmpty()) {
