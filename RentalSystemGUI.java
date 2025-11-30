0b0b5c8fd9c6bbe09c1e405a76caaa59d123ffd8
Anna Ogunkolade <giftedfavor12@gmail.com>
2025-11-29 23:11:08 -0500

    I also added aditional features like phone number and stuff.

--- RentalSystemGUI.java
+++ RentalSystemGUI.java
@@ -52,7 +52,7 @@ public class RentalSystemGUI extends Application {
         public Customer(String id, String name, String phone) {
             this.id = id;
             this.name = name;
-            this.phone = phone;
+            this.phone = phone; (here)
         }
 
         public String getId() { return id; }
